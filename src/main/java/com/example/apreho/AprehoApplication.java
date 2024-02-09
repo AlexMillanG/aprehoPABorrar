4a973435c88021e37dab469d285380cdfdc03932
AlexMillanG <132949276+AlexMillanG@users.noreply.github.com>
2024-02-09 14:25:54 -0600

    Esta bien facil we🙏🙏🙏

---
+++ src/main/java/com/example/apreho/AprehoApplication.java
@@ -0,0 +1,13 @@
+package com.example.apreho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AprehoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AprehoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/apreho/AprehoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apreho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AprehoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
